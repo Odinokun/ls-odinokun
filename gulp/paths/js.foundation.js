2f4679e809dab2bcf620295e10cea058bf472eb3
odinokun <odinokun@gmail.com>
2016-10-08 16:08:48 +0300

    did about.html with responsive & skills-row

--- gulp/paths/js.foundation.js
+++ gulp/paths/js.foundation.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = [
-  './bower_components/jquery/dist/jquery.min.js'
+  './bower_components/jquery/dist/jquery.min.js',
+  './source/vendors/jQuery-Knob-master/dist/jquery.knob.min.js'
 ];
