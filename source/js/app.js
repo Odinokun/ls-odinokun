ccf8b69cbcd4999c98e89c518baf94064657e7e3
odinokun <odinokun@gmail.com>
2016-10-09 19:08:33 +0300

    did blog with responsive

--- source/js/app.js
+++ source/js/app.js
@@ -13,7 +13,7 @@ $('.menu-icon').on('click', function() {
 
 //slowscroll
 $(function() {
-  $('a[href*=\\#]').on("click", function(e){
+  $('.arr-down__link[href*=\\#]').on("click", function(e){
      e.preventDefault();
      var anchor = $(this);
      $('html, body').stop().animate({
@@ -23,102 +23,138 @@ $(function() {
   return false;
 });
 
+//blog-menu-scroll
+$(function() {
+  $('.blog-aside__link[href*=\\#]').on("click", function(e){
+     e.preventDefault();
+     var anchor = $(this);
+     $('html, body').stop().animate({
+      scrollTop: $(anchor.attr('href')).offset().top - 30 + 'px'
+     }, 1000);
+  });
+  return false;
+});
 
-    $(function() {
-        $(".front").knob({
-          "min":0,
-          "max":100,
-          "fgColor":"#1056d1",
-          "skin":"tron",
-          "cursor":false,
-          "width":120,
-          "height":120
-        });
-    });
+//top-menu__icon
+$('.blog-aside__button').on('click', function() {
+  $('.blog-aside').toggleClass("active");
+  return false;
+});
 
-    $(function() {
-        $(".back").knob({
-          "min":0,
-          "max":100,
-          "fgColor":"#86a2d3",
-          "skin":"tron",
-          "cursor":false,
-          "width":120,
-          "height":120
-        });
-    });
+// nav-menu fixed
+// $(function() {
+//   var offset = $('.blog-aside__nav').offset();
+//   var topPadding = 30;
 
-    $(function() {
-        $(".workflow").knob({
-          "min":0,
-          "max":100,
-          "fgColor":"#1056d1",
-          "skin":"tron",
-          "cursor":false,
-          "width":120,
-          "height":120
-        });
-    });
+//   $(window).on('scroll', function() {
+//     if ($(window).scrollTop() > offset.top) {
+//         $('#blog-nav').addClass('active');
+//         $('#article-wrap').addClass('active');
+//     }
+//     else {
+//         $('#blog-nav').removeClass('active');
+//         $('#article-wrap').removeClass('active');
+//     }
+//   });
+// });
 
 
-    google.maps.event.addDomListener(window, 'load', init);
-    var map;
-    function init() {
-        var mapOptions = {
-            center: new google.maps.LatLng(50.436817, 30.491573),
-            zoom: 14,
-            zoomControl: true,
-            zoomControlOptions: {
-                style: google.maps.ZoomControlStyle.DEFAULT,
-            },
-            disableDoubleClickZoom: true,
-            mapTypeControl: true,
-            mapTypeControlOptions: {
-                style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-            },
-            scaleControl: true,
-            scrollwheel: false,
-            panControl: true,
-            streetViewControl: true,
-            draggable: true,
-            overviewMapControl: true,
-            overviewMapControlOptions: {
-                opened: false,
-            },
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            styles: [{"stylers":[{"hue":"#007fff"},{"saturation":89}]},{"featureType":"water","stylers":[{"color":"#ffffff"}]},{"featureType":"administrative.country","elementType":"labels","stylers":[{"visibility":"off"}]}],
-        }
-        var mapElement = document.getElementById('map');
-        var map = new google.maps.Map(mapElement, mapOptions);
-        var marker = new google.maps.Marker({
-            // Определяем позицию маркера
-            position: {
-                lat: 50.449817,
-                lng: 30.522973
-            },
-            map: map,
-            icon: 'assets/img/marker.png'
-        });
-        var locations = [
 
-        ];
-        for (i = 0; i < locations.length; i++) {
-            if (locations[i][1] == 'undefined') { description = ''; } else { description = locations[i][1]; }
-            if (locations[i][2] == 'undefined') { telephone = ''; } else { telephone = locations[i][2]; }
-            if (locations[i][3] == 'undefined') { email = ''; } else { email = locations[i][3]; }
-            if (locations[i][4] == 'undefined') { web = ''; } else { web = locations[i][4]; }
-            if (locations[i][7] == 'undefined') { markericon = ''; } else { markericon = locations[i][7]; }
-            marker = new google.maps.Marker({
-                icon: markericon,
-                position: new google.maps.LatLng(locations[i][5], locations[i][6]),
-                map: map,
-                title: locations[i][0],
-                desc: description,
-                tel: telephone,
-                email: email,
-                web: web
-            });
-            link = '';
-        }
+$(function() {
+  $(".front").knob({
+    "min":0,
+    "max":100,
+    "fgColor":"#1056d1",
+    "skin":"tron",
+    "cursor":false,
+    "width":120,
+    "height":120
+  });
+});
 
+$(function() {
+  $(".back").knob({
+    "min":0,
+    "max":100,
+    "fgColor":"#86a2d3",
+    "skin":"tron",
+    "cursor":false,
+    "width":120,
+    "height":120
+  });
+});
+
+$(function() {
+  $(".workflow").knob({
+    "min":0,
+    "max":100,
+    "fgColor":"#1056d1",
+    "skin":"tron",
+    "cursor":false,
+    "width":120,
+    "height":120
+  });
+});
+
+
+google.maps.event.addDomListener(window, 'load', init);
+var map;
+function init() {
+    var mapOptions = {
+        center: new google.maps.LatLng(50.436817, 30.491573),
+        zoom: 14,
+        zoomControl: true,
+        zoomControlOptions: {
+            style: google.maps.ZoomControlStyle.DEFAULT,
+        },
+        disableDoubleClickZoom: true,
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+        },
+        scaleControl: true,
+        scrollwheel: false,
+        panControl: true,
+        streetViewControl: true,
+        draggable: true,
+        overviewMapControl: true,
+        overviewMapControlOptions: {
+            opened: false,
+        },
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        styles: [{"stylers":[{"hue":"#007fff"},{"saturation":89}]},{"featureType":"water","stylers":[{"color":"#ffffff"}]},{"featureType":"administrative.country","elementType":"labels","stylers":[{"visibility":"off"}]}],
     }
+    var mapElement = document.getElementById('map');
+    var map = new google.maps.Map(mapElement, mapOptions);
+    var marker = new google.maps.Marker({
+        // Определяем позицию маркера
+        position: {
+            lat: 50.449817,
+            lng: 30.522973
+        },
+        map: map,
+        icon: 'assets/img/marker.png'
+    });
+    var locations = [
+
+    ];
+    for (i = 0; i < locations.length; i++) {
+        if (locations[i][1] == 'undefined') { description = ''; } else { description = locations[i][1]; }
+        if (locations[i][2] == 'undefined') { telephone = ''; } else { telephone = locations[i][2]; }
+        if (locations[i][3] == 'undefined') { email = ''; } else { email = locations[i][3]; }
+        if (locations[i][4] == 'undefined') { web = ''; } else { web = locations[i][4]; }
+        if (locations[i][7] == 'undefined') { markericon = ''; } else { markericon = locations[i][7]; }
+        marker = new google.maps.Marker({
+            icon: markericon,
+            position: new google.maps.LatLng(locations[i][5], locations[i][6]),
+            map: map,
+            title: locations[i][0],
+            desc: description,
+            tel: telephone,
+            email: email,
+            web: web
+        });
+        link = '';
+    }
+
+}
